29f4160c7b414ad17ddda5b886456e8c5c10612a
illia <echopro-create@users.noreply.github.com>
2026-06-23 00:52:08 +0200

    fix: resolve search-icon style leak causing preview icon display in wrong location

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://smart-hub.info',
   integrations: [sitemap()],
+  markdown: {
+    smartypants: false,
+  },
 });
